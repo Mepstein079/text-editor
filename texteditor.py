90c81ed6b3b9c43abc3b53b88f777649cfa83b41
Mepstein079 <133897268+Mepstein079@users.noreply.github.com>
2024-01-17 05:19:34 -0500

    Update texteditor.py

--- texteditor.py
+++ texteditor.py
@@ -2,9 +2,11 @@ from tkinter import *
 import tkinter as tk
 from tkinter.filedialog import asksaveasfilename
 
+
 # Creates the save function to be used later
 def save():
-    filepath = asksaveasfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    filepath = asksaveasfilename(defaultextension="txt", filetypes=[
+                                 ("Text Files", "*.txt"), ("All Files", "*.*")])
     if not filepath:
         return
     with open(filepath, "w") as output:
@@ -12,19 +14,28 @@ def save():
         output.write(text)
     text_window.title(f"Entitled - {filepath}")
 
+
 # creating the text window
 text_window = Tk()
 text_window.geometry('600x600')
 text_window.title("Matt's Text Editor")
 
 # Will create the label for the heading
-heading = Label(text_window, text="Text Editor", font=('bold',20), bg='light grey')
+heading = Label(text_window, text="Text Editor",
+                font=('bold', 20), bg='light grey')
 heading.pack()
 
 # Creates the scroll bar on the right side along with the area where the text will be
-scroll = Scrollbar(text_window).pack(side=RIGHT, fill=Y)
-editor = Text(text_window, width=400, height=450, yscrollcommand=scroll.set).pack(fill=BOTH)
+scroll = Scrollbar(text_window)
+scroll.pack(side=RIGHT, fill=Y)
+editor = Text(text_window, width=400, height=450, yscrollcommand=scroll.set)
+editor.pack(fill=BOTH)
 scroll.config(command=editor.yview)
 
 # The button where the user can save their text
-button = Button(text_window, text='Save', font=('normal', 10), command=save, bg='yellow')
+save_button = Button(text_window, text='Save', font=(
+    'normal', 10), command=save, bg='light blue')
+save_button.place(x=270, y=520)
+
+
+text_window.mainloop()
