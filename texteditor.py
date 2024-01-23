f76955c5a0815b6fb84c5592730c7b9ed79b6d82
Mepstein079 <133897268+Mepstein079@users.noreply.github.com>
2024-01-23 11:28:31 -0500

    Update texteditor.py
    Added a word counter and began trying to keep the save and load button's position static

--- texteditor.py
+++ texteditor.py
@@ -15,8 +15,11 @@ def save():
     text_window.title(f"Entitled - {filepath}")
 
 
+# User chooses a text file to load, which will delete whatever is currently on the text editor
+# Will replace the current contents with the new text file's contents
 def load():
-    filepath = askopenfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt")], )
+    filepath = askopenfilename(defaultextension="txt", filetypes=[
+                               ("Text Files", "*.txt")], )
     if not filepath:
         return
 
@@ -25,6 +28,14 @@ def load():
         editor.delete(1.0, END)
         editor.insert(INSERT, lines)
     text_window.title(f"Entitled - {filepath}")
+
+
+# Will count the user's words to display them
+def word_counter():
+    text = editor.get(1.0, END)
+    words = text.split()
+    word_count_label.config(text=f"Word Count: {len(words)}")
+
 
 # creating the text window
 text_window = Tk()
@@ -46,9 +57,23 @@ scroll.config(command=editor.yview)
 # The button where the user can save their text
 save_button = Button(text_window, text="Save", font=(
     "normal", 10), command=save, bg="light blue")
-save_button.place(x=270, y=520)
+save_bottom_width = save_button.winfo_reqwidth()
+save_button.place(relx=0.45, rely=.9, anchor="s", x=save_bottom_width/2 + 10)
 
-load_button = Button(text_window, text="Load", font=("normal", 10), command=load, bg="red")
-load_button.place(x=330, y=520)
+
+# The button to for the user to load their text file
+load_button = Button(text_window, text="Load", font=(
+    "normal", 10), command=load, bg="red")
+load_bottom_width = load_button.winfo_reqwidth()
+load_button.place(relx=0.53, rely=0.9, anchor="s", x=load_bottom_width/2 - 10)
+
+
+# Area for the user's word count to show
+word_count_label = Label(
+    text_window, text="Word Count: 0", font=("normal", 10))
+word_count_label.place(relx=0, rely=1, anchor="sw")
+
+# Calls the word_counter function at each key release
+editor.bind("<KeyRelease>", lambda event: text_window.after(150, word_counter))
 
 text_window.mainloop()
