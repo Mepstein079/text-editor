ca2d8073ee2d20a6892cccf556211d39e3a4d226
Mepstein079 <133897268+Mepstein079@users.noreply.github.com>
2024-01-24 16:05:57 -0500

    Adding features and plans
    Added a document for me to track what I want to add in the future, and started working on having an options window to keep the ui less cluttered

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A simple text editor to help me experiment with the tkinter module
 
 For Windows change Load button and Save button relx to 0.53 and 0.45 respectively.
 
-For Mac change Load button and Save button relx to 0.51 and 0.41 respectively/
+For Macbook Pro change Load button and Save button relx to 0.51 and 0.41 respectively/
--- texteditor.py
+++ texteditor.py
@@ -5,8 +5,10 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 
 # Creates the save function to be used later
 def save():
-    filepath = asksaveasfilename(defaultextension="txt", filetypes=[
-                                 ("Text Files", "*.txt")])
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
     if not filepath:
         return
     with open(filepath, "w") as output:
@@ -18,8 +20,10 @@ def save():
 # User chooses a text file to load, which will delete whatever is currently on the text editor
 # Will replace the current contents with the new text file's contents
 def load():
-    filepath = askopenfilename(defaultextension="txt", filetypes=[
-                               ("Text Files", "*.txt")], )
+    filepath = askopenfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
     if not filepath:
         return
 
@@ -37,40 +41,67 @@ def word_counter():
     word_count_label.config(text=f"Word Count: {len(words)}")
 
 
+# Calculates the option's location
+def calculate_options_location():
+    x = text_window.winfo_rootx() + show_options.winfo_x()
+    y = text_window.winfo_rooty() + +show_options.winfo_y() + \
+        show_options.winfo_height()
+    return x, y
+
+
+# Will be called to show the Toplevel window where the user can pick new options
+def show_option_window():
+    # Creates the window
+    option_window = tk.Toplevel(text_window)
+
+    # Sets the position, it not being resizable, and not show window manager
+    option_window.geometry("+40+55")
+    option_window.resizable(False, False)
+    option_window.overrideredirect(True)
+
+    option_window.update()
+
+    x, y = calculate_options_location()
+    option_window.geometry(f"+{x}+{y}")
+
+    text_window.bind("<Configure>", lambda event: option_window.geometry(
+        f"+{calculate_options_location()[0]}+{calculate_options_location()[1]}"))
+
+    # The button where the user can save their text
+    save_button = Button(
+        option_window, text="Save", font=("normal", 10), command=save
+    )
+    save_button.pack()
+
+    # The button to for the user to load their text file
+    load_button = Button(
+        option_window, text="Load", font=("normal", 10), command=load
+    )
+    load_button.pack()
+
+
 # creating the text window
 text_window = Tk()
 text_window.geometry("600x600")
 text_window.title("Matt's Text Editor")
 
-# Will create the label for the heading
-heading = Label(text_window, text="Text Editor",
-                font=('bold', 20), bg='light grey')
-heading.pack()
+
+# Creates a button in the text_window to show the option_window
+show_options = tk.Button(text_window, text="Options",
+                         command=show_option_window, font=("normal", 10))
+show_options.pack(anchor="nw")
 
 # Creates the scroll bar on the right side along with the area where the text will be
 scroll = Scrollbar(text_window)
 scroll.pack(side=RIGHT, fill=Y)
-editor = Text(text_window, width=400, height=450, yscrollcommand=scroll.set)
+editor = Text(text_window, width=400, height=450, yscrollcommand=scroll.set, bg="black", font=("Helvetica", 12), fg="white")
 editor.pack(fill=BOTH)
 scroll.config(command=editor.yview)
-
-# The button where the user can save their text
-save_button = Button(text_window, text="Save", font=(
-    "normal", 10), command=save, bg="light blue")
-save_bottom_width = save_button.winfo_reqwidth()
-save_button.place(relx=0.45, rely=.9, anchor="s", x=save_bottom_width/2)
-
-
-# The button to for the user to load their text file
-load_button = Button(text_window, text="Load", font=(
-    "normal", 10), command=load, bg="red")
-load_bottom_width = load_button.winfo_reqwidth()
-load_button.place(relx=0.53, rely=0.9, anchor="s", x=load_bottom_width/2)
 
 
 # Area for the user's word count to show
 word_count_label = Label(
-    text_window, text="Word Count: 0", font=("normal", 10))
+    text_window, text="Word Count: 0", font=("normal", 10), bg="black", fg="white")
 word_count_label.place(relx=0, rely=1, anchor="sw")
 
 # Calls the word_counter function at each key release
